fa0aba8e5a2ed8da14949ad33b3dbe4c4e840ab9
Paulo Eduardo de Sordi Gomes <pauloeduardodesordigomes@gmail.com>
2021-06-23 16:10:35 -0300

    Add exercise 6

---
+++ bloco4/exercises/introducao-a-javascript-e-logica-de-programacao_2/6.js
@@ -0,0 +1,9 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+var numOfOdd = 0
+
+for(number of numbers){
+  if(number % 2 != 0) numOfOdd += 1
+}
+
+numOfOdd > 0? console.log(numOfOdd) : console.log("nenhum valor impar encontrado")
