12e549a7b812b9a796cf8c22cf9cd60983241451
Paulo Eduardo de Sordi Gomes <pauloeduardodesordigomes@gmail.com>
2021-06-24 19:03:26 -0300

    Add exercise 4

---
+++ bloco4/exercises/introducao-a-javascript-e-logica-de-programacao_3/4.js
@@ -0,0 +1,19 @@
+let n = 5
+let n1 = 1
+
+if (n % 2 !== 0){
+  let lineBreak = (n + 1) / 2
+  let spaces = lineBreak - 1
+  for(let i = 0; i < lineBreak; i += 1){
+    let string = ''
+    for(let i1 = 0; i1 < spaces; i1 += 1){
+      string += ' '
+    }
+    spaces -= 1
+    for(let i2 = 0; i2 < n1; i2 += 1){
+      string += '*'
+    }
+    n1 += 2
+    console.log(string)
+  }
+}
