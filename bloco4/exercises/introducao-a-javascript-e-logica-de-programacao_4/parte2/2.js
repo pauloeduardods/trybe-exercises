be1201792f577c56c200b4d7bab50217c1ceb54b
Paulo Eduardo de Sordi Gomes <pauloeduardodesordigomes@gmail.com>
2021-06-25 19:21:20 -0300

    Add exercise 3 Parte2

--- bloco4/exercises/introducao-a-javascript-e-logica-de-programacao_4/parte2/2.js
+++ bloco4/exercises/introducao-a-javascript-e-logica-de-programacao_4/parte2/2.js
@@ -2,8 +2,8 @@ let array = [2, 3, 6, 7, 10, 1];
 
 const biggerIndex = array => {
   let bigger
-  for(index in array){
-    !bigger? bigger = array[index] : bigger < array[index]? bigger = array[index] : bigger
+  for(number of array){
+    !bigger? bigger = number : bigger < number? bigger = number : bigger
   };
   return array.indexOf(bigger)
 };
