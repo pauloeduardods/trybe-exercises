463eb3cceef71bb3fe64ad9504d244c92b89c902
Paulo Eduardo de Sordi Gomes <pauloeduardodesordigomes@gmail.com>
2021-06-26 17:29:29 -0300

    Add exercise 7 Parte2

---
+++ bloco4/exercises/introducao-a-javascript-e-logica-de-programacao_4/parte2/7.js
@@ -0,0 +1,12 @@
+const verifier = (word, endsWith) => {
+  let endWithIndex = 0;
+  for(let i = word.length - endsWith.length; i <= word.length - 1; i += 1){
+    if(word[i] !== endsWith[endWithIndex]){
+      return false;
+    };
+    endWithIndex += 1;
+  };
+  return true;
+};
+
+console.log(verifier('trybe', 'be'))
