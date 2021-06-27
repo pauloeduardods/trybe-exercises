9b1984523a5802e96842ddaa751e49ce0b2b7ed5
Paulo Eduardo de Sordi Gomes <pauloeduardodesordigomes@gmail.com>
2021-06-27 00:29:18 -0300

    Remove unecessary verifications in Bonus 1 block 4

--- bloco4/exercises/introducao-a-javascript-e-logica-de-programacao_4/Bonus/Roman-Numerals-To-Decimals.js
+++ bloco4/exercises/introducao-a-javascript-e-logica-de-programacao_4/Bonus/Roman-Numerals-To-Decimals.js
@@ -12,25 +12,34 @@ const romanNumeralsToDecimals = string => {
   string = string.toLocaleLowerCase();
   let result = 0;
   for(let i = 0; i < string.length; i += 1){
-    let currentNum = romanNumerals[string[i]]
-    let nextNum = romanNumerals[string[i + 1]]
-    let nextOfNextNum = romanNumerals[string[i + 2]]
-    if(nextNum === undefined) {
-      result += currentNum;
+    if(romanNumerals[string[i + 1]] === undefined) {
+      result += romanNumerals[string[i]];
     };
-    if(currentNum === nextNum){
-      if(nextNum === nextOfNextNum){
-        result += currentNum + nextNum + nextOfNextNum;
-        i += 2;
-      }else{
-        result += currentNum + nextNum;
-        i += 1;
-      };
-    };
-    if(currentNum > nextNum) result += currentNum;
-    if(currentNum < nextNum) result -= currentNum;
+    if(romanNumerals[string[i]] >= romanNumerals[string[i + 1]]) result += romanNumerals[string[i]];
+    if(romanNumerals[string[i]] < romanNumerals[string[i + 1]]) result -= romanNumerals[string[i]];
   };
   return result;
 };
 
-console.log(romanNumeralsToDecimals('MMMCCXXXVIII'))
+console.log(romanNumeralsToDecimals('X'))//10
+console.log(romanNumeralsToDecimals('XI'))//11
+console.log(romanNumeralsToDecimals('XII'))//12
+console.log(romanNumeralsToDecimals('XIII'))//13
+console.log(romanNumeralsToDecimals('XIV'))//14
+console.log(romanNumeralsToDecimals('XV'))//15
+console.log(romanNumeralsToDecimals('XVI'))//16
+console.log(romanNumeralsToDecimals('XVII'))//17
+console.log(romanNumeralsToDecimals('XVIII'))//18
+console.log(romanNumeralsToDecimals('XIX'))//19
+console.log(romanNumeralsToDecimals('XX'))//20
+console.log(romanNumeralsToDecimals('CMI'))//901
+console.log(romanNumeralsToDecimals('CMXCIII'))//993
+console.log(romanNumeralsToDecimals('CMXCIV'))//994
+console.log(romanNumeralsToDecimals('CMXCIX'))//999
+console.log(romanNumeralsToDecimals('DCLXVI'))//666
+console.log(romanNumeralsToDecimals('MMMCCLXXIII'))//3273: 
+console.log(romanNumeralsToDecimals('MMMCCXXXVIII'))//3238
+console.log(romanNumeralsToDecimals('MDCCLX'))//1760
+console.log(romanNumeralsToDecimals('MDCCCXI'))//1811
+console.log(romanNumeralsToDecimals('MDCCCLIII'))//1853
+console.log(romanNumeralsToDecimals('DCCLXXXVIII'))//788
