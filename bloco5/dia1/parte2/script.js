e4227edafb288180dbe5e468cb20ce5fa887e91b
Paulo Eduardo de Sordi Gomes <pauloeduardodesordigomes@gmail.com>
2021-06-28 15:43:30 -0300

    Add fix 1

---
+++ bloco5/dia1/parte2/script.js
@@ -0,0 +1,9 @@
+const paragraph = document.getElementById("paragraph");
+paragraph.style.color = "red";
+const title = document.getElementById("page-title");
+title.innerText = 'Salve salve familia';
+const secondParagraph = document.getElementById("second-paragraph");
+secondParagraph.innerText = 'outro';
+secondParagraph.style.color = "green";
+const subtitle = document.getElementById("subtitle");
+subtitle.style.color = 'magenta';
