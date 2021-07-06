d4f60a0da0c656412d7c47d64c44e1b810572d55
Paulo Eduardo de Sordi Gomes <pauloeduardodesordigomes@gmail.com>
2021-07-06 15:51:28 -0300

    Add line-height modifier

--- Fundamentos/bloco5/exercises/dia-4-javascript-web-storage/script.js
+++ Fundamentos/bloco5/exercises/dia-4-javascript-web-storage/script.js
@@ -28,14 +28,9 @@ function textSize() {
   function setTextSize() {
     let textSizeSaved = localStorage.getItem('text-size');
     let mainContent = document.querySelector('.body');
-    if (!textSizeSaved) {
-      textSizeInput.placeholder = `Tamanho do texto atual ${defaultTextSize}`;
-      mainContent.style.fontSize = `${defaultTextSize}px`;
-    }
-    else {
-      textSizeInput.placeholder = `Tamanho do texto atual ${textSizeSaved}`;
-      mainContent.style.fontSize = `${textSizeSaved}px`;
-    }
+    if (textSizeSaved === null) textSizeSaved = defaultTextSize;
+    textSizeInput.placeholder = `Tamanho do texto atual ${textSizeSaved}`;
+    mainContent.style.setProperty('font-size', `${textSizeSaved}px`);
   }
   setTextSize();
   textSizeInput.addEventListener('keypress', event => {
@@ -49,3 +44,24 @@ function textSize() {
   });
 }
 textSize();
+
+function lineHeight() {
+  const defaultValue = 'normal';
+  let lineHeightInput = document.getElementById('line-height');
+  function setLineHeight() {
+    let mainContent = document.querySelector('.body');
+    let lineHeightSaved = localStorage.getItem('line-height');
+    mainContent.style.setProperty('line-height', lineHeightSaved);
+  }
+  setLineHeight();
+  lineHeightInput.addEventListener('keypress', event => {
+    if (event.key === 'Enter') {
+      let newValue = lineHeightInput.value;
+      lineHeightInput.value = '';
+      if (isNaN(newValue) === false && newValue !== '') localStorage.setItem('line-height', newValue);
+      else localStorage.setItem('line-height', defaultValue);
+      setLineHeight();
+    }
+  })
+}
+lineHeight();
