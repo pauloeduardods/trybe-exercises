1feed7d6c91fd542d6c6e29d292a854070df8ba9
Paulo Eduardo de Sordi Gomes <pauloeduardodesordigomes@gmail.com>
2021-07-06 11:43:16 -0300

    add dark mode to a single function

--- Fundamentos/bloco5/exercises/dia-4-javascript-web-storage/script.js
+++ Fundamentos/bloco5/exercises/dia-4-javascript-web-storage/script.js
@@ -1,23 +1,23 @@
 function darkMode() {
-  let darkModeState = localStorage.getItem('dark-mode');
-  let root = document.documentElement;
-  let darkModeText = document.getElementById('dark-mode-button');
-  if (darkModeState === 'false' || !darkModeState) {
-    root.className = 'light';
-    darkModeText.innerText = 'Dark Mode: OFF';
+  function setTheme() {
+    let darkModeState = localStorage.getItem('dark-mode');
+    let root = document.documentElement;
+    let darkModeText = document.getElementById('dark-mode-button');
+    if (darkModeState === 'false' || !darkModeState) {
+      root.className = 'light';
+      darkModeText.innerText = 'Dark Mode: OFF';
+    }
+    else {
+      root.className = 'dark'
+      darkModeText.innerText = 'Dark Mode: ON';
+    }
   }
-  else {
-    root.className = 'dark'
-    darkModeText.innerText = 'Dark Mode: ON';
-  }
+  setTheme();
+  document.getElementById('dark-mode-button').addEventListener('click', function() {
+    let darkModeState = localStorage.getItem('dark-mode');
+    if (!darkModeState || darkModeState === 'false') localStorage.setItem('dark-mode', true);
+    else localStorage.setItem('dark-mode', false);
+    setTheme();
+  });
 }
 darkMode();
-
-document.getElementById('dark-mode-button').addEventListener('click', setDarkMode);
-
-function setDarkMode() {
-  let darkModeState = localStorage.getItem('dark-mode');
-  if (!darkModeState || darkModeState === 'false') localStorage.setItem('dark-mode', true);
-  else localStorage.setItem('dark-mode', false);
-  darkMode();
-}
