55b928c95a0b76536944c97b914cdb1fc6afb37f
Paulo Eduardo de Sordi Gomes <pauloeduardodesordigomes@gmail.com>
2021-07-23 16:07:53 -0300

    Add exercise 5 part2

--- Fundamentos/bloco7/exercises/dia-2-javascript-es6-objects/part2.js
+++ Fundamentos/bloco7/exercises/dia-2-javascript-es6-objects/part2.js
@@ -26,4 +26,4 @@ const objectLength = object => Object.keys(object).length;
 
 const objectValues = object => Object.values(object);
 
-
+const allLessons = Object.assign({}, {lesson1, lesson2, lesson3});
