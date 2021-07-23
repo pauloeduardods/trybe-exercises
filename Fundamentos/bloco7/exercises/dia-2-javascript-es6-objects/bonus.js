659ca758efe0bf10c583d069c63a2821b5b9d842
Paulo Eduardo de Sordi Gomes <pauloeduardodesordigomes@gmail.com>
2021-07-23 18:11:15 -0300

    Add exercise 2 bonus

--- Fundamentos/bloco7/exercises/dia-2-javascript-es6-objects/bonus.js
+++ Fundamentos/bloco7/exercises/dia-2-javascript-es6-objects/bonus.js
@@ -30,3 +30,29 @@ const studentsClass = (object, subject) => {
 };
 
 console.log(studentsClass(allLessons, 'Matemática'));
+
+const teacherSubject = (object, name) => {
+  const lessons = Object.keys(object);
+  const resultObj = {};
+  const resultArr = [];
+  const verifier = [];
+  let numberOfStudents = 0;
+  for (let lesson of lessons) {
+    if (object[lesson].professor === name) {
+      resultArr.push(object[lesson].materia);
+      Object.assign(resultObj, object[lesson]);
+      resultObj.materia = resultArr;
+    }
+  }
+  
+  for (let subject of resultArr) {
+    if (!verifier.includes(subject)) {
+      verifier.push(subject);
+      numberOfStudents += studentsClass(object, subject);
+    }
+  }
+  resultObj.numeroEstudantes = numberOfStudents;
+  return resultObj;
+};
+
+console.log(teacherSubject(allLessons, 'Maria Clara'));
