7d87388ab2c2c2c159ab5df4ced6dd8be4836943
Paulo Eduardo de Sordi Gomes <pauloeduardodesordigomes@gmail.com>
2021-07-23 14:57:15 -0300

    Add exeples 1 - 4

---
+++ Fundamentos/bloco7/dia2/exemplos.js
@@ -0,0 +1,28 @@
+const países = {
+  França: 'Paris',
+  Brasil: 'Brasília',
+  Espanha: 'Madrid',
+  Portugal: 'Lisboa',
+};
+const pairKeyValue = Object.entries(países); //retorna 1 array com arrays em q o index 0 é a chave e o 1 é o valor 
+console.log(pairKeyValue);
+
+const student = {
+  Html: 'Muito Bom',
+  Css: 'Bom',
+  JavaScript: 'Ótimo',
+  SoftSkill: 'Ótimo',
+};
+
+const listSkillsValuesWithValues = (student) => Object.values(student);
+
+console.log(listSkillsValuesWithValues(student));
+
+const listSkills = (student) => {
+  const arrayOfSkills = Object.keys(student);
+  for(index in arrayOfSkills){
+    console.log(`${arrayOfSkills[index]}, Nível: ${student[arrayOfSkills[index]]}`);
+  }
+};
+
+console.log(listSkills(student));
