ea5f6d24897a810afa3b8535de33f29572353e79
Paulo Eduardo de Sordi Gomes <pauloeduardodesordigomes@gmail.com>
2021-07-28 15:20:50 -0300

    Add exercise 3

--- Fundamentos/bloco8/exercicios/dia-1-javascript-es6-introducao-a-higher-order-functions/exercises.js
+++ Fundamentos/bloco8/exercicios/dia-1-javascript-es6-introducao-a-higher-order-functions/exercises.js
@@ -20,3 +20,21 @@ const result = (number, func) => {
 }
 
 console.log(result(3, compare));
+
+const answersChecker = (arrayAnswers, arrayStudentAnswers) => {
+  let result = 0;
+  for (let i = 0; i < arrayAnswers.length; i += 1) {
+    if (arrayStudentAnswers[i] !== 'N.A') {
+      if (arrayAnswers[i] === arrayStudentAnswers[i]) result += 1;
+      else result -= 0.5;
+    }
+  }
+  return result;
+}
+
+const testResult = (answers, studentAnswers, comparator) => comparator(answers, studentAnswers);
+
+const RIGHT_ANSWERS = ['A', 'C', 'B', 'D', 'A', 'A', 'D', 'A', 'D', 'C'];
+const STUDENT_ANSWERS = ['A', 'N.A', 'B', 'D', 'A', 'C', 'N.A', 'A', 'D', 'B'];
+
+console.log(testResult(RIGHT_ANSWERS, STUDENT_ANSWERS, answersChecker));
