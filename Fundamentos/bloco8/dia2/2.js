3abbe7921ea8ef7a9d7d501277f55dfd9ec9f828
Paulo Eduardo de Sordi Gomes <pauloeduardodesordigomes@gmail.com>
2021-07-29 14:47:16 -0300

    Add fixation 3

--- Fundamentos/bloco8/dia2/2.js
+++ Fundamentos/bloco8/dia2/2.js
@@ -3,3 +3,9 @@ const numbers = [19, 21, 30, 3, 45, 22, 15];
 const findDivisibleBy3And5 = (number) => number % 3 === 0 && number % 5 === 0;
 
 console.log(numbers.find(findDivisibleBy3And5))
+
+const names = ['João', 'Irene', 'Fernando', 'Maria'];
+
+const findNameWithFiveLetters = (name) => name.length === 5; 
+
+console.log(names.find(findNameWithFiveLetters));
