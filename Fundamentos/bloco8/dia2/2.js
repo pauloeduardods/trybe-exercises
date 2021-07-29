1d78d53c57070b2dd4ddac693d5eff3c920ee41d
Paulo Eduardo de Sordi Gomes <pauloeduardodesordigomes@gmail.com>
2021-07-29 14:50:26 -0300

    Add fixation 2.3

--- Fundamentos/bloco8/dia2/2.js
+++ Fundamentos/bloco8/dia2/2.js
@@ -9,3 +9,13 @@ const names = ['João', 'Irene', 'Fernando', 'Maria'];
 const findNameWithFiveLetters = (name) => name.length === 5; 
 
 console.log(names.find(findNameWithFiveLetters));
+
+const musicas = [
+  { id: '31031685', title: 'Partita in C moll BWV 997' },
+  { id: '31031686', title: 'Toccata and Fugue, BWV 565' },
+  { id: '31031687', title: 'Chaconne, Partita No. 2 BWV 1004' },
+]
+
+const findMusic = (id) => musicas.find(music => id === music.id);
+
+console.log(findMusic('31031685'))
