7a2c924ae94e5a24a1761056f85d65b0a07e612c
Paulo Eduardo de Sordi Gomes <pauloeduardodesordigomes@gmail.com>
2021-07-29 15:04:49 -0300

    Add fixation 3.2

--- Fundamentos/bloco8/dia2/3.js
+++ Fundamentos/bloco8/dia2/3.js
@@ -1,7 +1,18 @@
 const names = ['Mateus', 'José', 'Ana', 'Cláudia', 'Bruna'];
 
-const hasName = (arr, name) => {
-  return arr.some((currentName) => currentName === name);
-}
+const hasName = (arr, name) => arr.some((currentName) => currentName === name);
 
 console.log(hasName(names, 'Ana'));
+
+
+const people = [
+  { name: 'Mateus', age: 18 },
+  { name: 'José', age: 16 },
+  { name: 'Ana', age: 23 },
+  { name: 'Cláudia', age: 20 },
+  { name: 'Bruna', age: 19 },
+];
+
+const verifyAges = (arr, minimumAge) => arr.every(value => value.age >= minimumAge);
+
+console.log(verifyAges(people, 18));
