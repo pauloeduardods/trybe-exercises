0ff179e2f38de47e80df25b686c163fe76f02f06
Paulo Eduardo de Sordi Gomes <pauloeduardodesordigomes@gmail.com>
2021-08-02 16:58:24 -0300

    Create reduce hof

---
+++ Fundamentos/bloco9/exercicios/dia-1-javascript-assincrono-e-callbacks/REDUCE.js
@@ -0,0 +1,8 @@
+const reduce = (array, callback, initial) => {
+  array.forEach(cur => initial = callback(initial, cur));
+  return initial;
+}
+
+const arr = [1, 2, 3];
+
+console.log(reduce(arr, (acc, cur) => acc + cur, 0));
