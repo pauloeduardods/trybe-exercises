cac89139605f61bd58ca8e16c1ad15a2beda3809
Paulo Eduardo de Sordi Gomes <pauloeduardodesordigomes@gmail.com>
2021-08-03 19:25:08 -0300

    Add exercise 3 - 4

--- Fundamentos/bloco9/exercicios/dia-2-javascript-promises/2-5.js
+++ Fundamentos/bloco9/exercicios/dia-2-javascript-promises/2-5.js
@@ -1,3 +1,5 @@
+const { rejects } = require("assert");
+
 const promise = new Promise((resolve, reject) => {
   const arr = [];
   for (let i = 0; i < 10; i += 1) {
@@ -5,8 +7,15 @@ const promise = new Promise((resolve, reject) => {
   }
   const result = arr.reduce((acc, cur) => acc + cur, 0);
   if (result < 8000) return resolve(result);
-  reject(result);
+  reject(new Error(result));
 });
 
-promise.then(result => console.log(result))
-  .catch(result => console.log(`Error: ${result}`));
+promise.then(result => {
+  const dividers = [2, 3, 5, 10];
+  return dividers.map(element => result / element);
+})
+  .catch(error => console.log('É mais de oito mil! Essa promise deve estar quebrada!' + error));
+
+const sum = new Promise((resolve, reject) => {
+  
+})
