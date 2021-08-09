bc1e277ee9ad41d0a1a100e0eb2ad252f3a2723c
Paulo Eduardo de Sordi Gomes <pauloeduardodesordigomes@gmail.com>
2021-08-09 18:24:00 -0300

    exercise 2.3 part2

--- Fundamentos/bloco10/exercicios/dia-1-primeiros-passos-no-jest/techlist.js
+++ Fundamentos/bloco10/exercicios/dia-1-primeiros-passos-no-jest/techlist.js
@@ -1,3 +1,3 @@
-const techList = (arr, name) => arr.sort().map((tech) => ({ tech, name }));
+const techList = (arr, name) => arr.length === 0 ? 'Vazio!' : arr.sort().map((tech) => ({ tech, name }));
 
 module.exports = {techList};
