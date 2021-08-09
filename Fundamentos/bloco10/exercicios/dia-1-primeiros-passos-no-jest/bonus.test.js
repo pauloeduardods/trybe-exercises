7734c1a01f70ed427483437d9abe1bd0bd595a8f
Paulo Eduardo de Sordi Gomes <pauloeduardodesordigomes@gmail.com>
2021-08-09 19:14:18 -0300

    Add exercise 3 bonus

--- Fundamentos/bloco10/exercicios/dia-1-primeiros-passos-no-jest/bonus.test.js
+++ Fundamentos/bloco10/exercicios/dia-1-primeiros-passos-no-jest/bonus.test.js
@@ -12,8 +12,12 @@ describe('Bonus', () => {
   });
   it ('id not find', () => {
     expect(() => searchEmployee('32312', 'sla')).toThrowError('ID não identificada');
-  })
+  });
   it ('info not find', () => {
     expect(() => searchEmployee('1256-4', 'macaco')).toThrowError('Informação indisponível');
-  })
+  });
+  it ('find id and detail', () => {
+    expect(searchEmployee('4678-2', 'firstName')).toBe('Paul');
+    expect(searchEmployee('1256-4', 'specialities')).toEqual(['Hooks', 'Context API', 'Tailwind CSS']);
+  });
 })
