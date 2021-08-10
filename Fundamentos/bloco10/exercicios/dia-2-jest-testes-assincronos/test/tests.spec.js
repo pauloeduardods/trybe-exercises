ab64abb72c68e4074ad3c7c53f4941cd43fcf044
Paulo Eduardo de Sordi Gomes <pauloeduardodesordigomes@gmail.com>
2021-08-10 16:00:17 -0300

    Add exericise 2

--- Fundamentos/bloco10/exercicios/dia-2-jest-testes-assincronos/test/tests.spec.js
+++ Fundamentos/bloco10/exercicios/dia-2-jest-testes-assincronos/test/tests.spec.js
@@ -1,4 +1,5 @@
 const uppercase = require('../src/1');
+const getUserName = require('../src/2-3');
 
 describe('teste exercicio 1 uppercase async', () => {
   it('uppercase ola mundo must be OLA MUNDO', (done) => {
@@ -9,6 +10,16 @@ describe('teste exercicio 1 uppercase async', () => {
       } catch (error) {
         done(error)
       }
-    }))
+    }));
+  });
+});
+
+describe('teste exercicio 2-3 findUserById and getUserName', () => {
+  it('getUserName find user', () => (
+    getUserName(1).then((res) => expect(res).toEqual('Mark'))
+  ));
+  it('getUserName not find user', () => {
+    expect.assertions(1);
+    return getUserName(10).catch((error) => expect(error).toEqual(new Error('User with 10 not found.')))
   })
 });
