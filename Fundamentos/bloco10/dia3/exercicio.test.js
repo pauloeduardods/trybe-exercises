f3de5f9219bdb7032420eb1623545c7eff098725
Paulo Eduardo de Sordi Gomes <pauloeduardodesordigomes@gmail.com>
2021-08-18 16:37:56 -0300

    bomba

---
+++ Fundamentos/bloco10/dia3/exercicio.test.js
@@ -0,0 +1,15 @@
+let { somar, subtrair, multiplicar, dividir } = require('./exercicio');
+
+describe('exercicios', () => {
+  it('exercicio1', () => {
+    expect(subtrair(1, 2)).toBe(-1);
+  });
+  it('exercicio2', () => {
+    multiplicar = jest.fn().mockImplementation(() => 10);
+    expect(multiplicar(10, 29)).toBe(10);
+  });
+  it('exercicio3', () => {
+    somar = jest.fn().mockImplementation((a, b) => (a + b));
+    expect(somar(1, 2)).toBe(3);
+  })
+});
