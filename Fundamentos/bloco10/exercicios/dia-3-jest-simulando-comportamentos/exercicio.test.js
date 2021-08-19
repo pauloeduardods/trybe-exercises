6f81a59af9266f792c1b30202fa84507ffd58774
Paulo Eduardo de Sordi Gomes <pauloeduardodesordigomes@gmail.com>
2021-08-19 10:53:16 -0300

    Add exercise 5

--- Fundamentos/bloco10/exercicios/dia-3-jest-simulando-comportamentos/exercicio.test.js
+++ Fundamentos/bloco10/exercicios/dia-3-jest-simulando-comportamentos/exercicio.test.js
@@ -1,4 +1,4 @@
-const exercicios = require('./exercicio');
+let exercicios = require('./exercicio');
 
 describe('exercises', () => {
   it('exercicio1', () => {
@@ -18,23 +18,36 @@ describe('exercises', () => {
     exercicios.randomNumber = jest.fn().mockImplementation((a, b, c) => a * b * c);
     expect(exercicios.randomNumber(1,2,3)).toBe(6);
     exercicios.randomNumber.mockReset();
-    expect(exercicios.randomNumber()).not.toBe(101);
+    expect(exercicios.randomNumber()).not.toBe(6);
   });
-  describe('exercicio4', () => {
+  describe('exercicio4 - 5', () => {
     it('func1', () => {
+      const first = jest
+        .spyOn(exercicios, "func1")
+        .mockImplementation(a => a.toLowerCase());
+
+      expect(first("UPPERCASE")).toBe("uppercase");
+      expect(first).toHaveBeenCalled();
+      expect(first).toHaveBeenCalledTimes(1);
+      expect(first).toHaveBeenCalledWith("UPPERCASE");
+
+      exercicios.func1.mockRestore();
+
+      expect(exercicios.func1("lowercase")).toBe("LOWERCASE");
+
       expect(exercicios.func1('olaa')).toBe('OLAA');
-      exercicios.func1 = jest.fn().mockImplementationOnce((string) => string.toLowerCase());
+      exercicios.func1 = jest.fn().mockImplementation((string) => string.toLowerCase());
       expect(exercicios.func1('OLAA')).toBe('olaa');
     });
     it('func2', () => {
       expect(exercicios.func2('string')).toBe('s');
-      exercicios.func2 = jest.fn().mockImplementation((string) => string[string.length - 1]);
+      const mock = jest.spyOn(exercicios, 'func2').mockImplementation((string) => string[string.length - 1]);
       expect(exercicios.func2('string')).toBe('g');
     });
     it('func3', () => {
       expect(exercicios.func3('hello', ' world')).toBe('hello world');
       exercicios.func3 = jest.fn().mockImplementation((a, b, c) => a + b + c);
-      expect(exercicios.func3('azy', 'bzy', 'czy')).toBe('azybzyczy')
+      expect(exercicios.func3('azy', 'bzy', 'czy')).toBe('azybzyczy');
     });
   });
 });
