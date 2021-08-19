ee0fde71c32dca10a3084c81f9d45b766b0cc875
Paulo Eduardo de Sordi Gomes <pauloeduardodesordigomes@gmail.com>
2021-08-19 09:53:40 -0300

    Add exercise 4

--- Fundamentos/bloco10/exercicios/dia-3-jest-simulando-comportamentos/exercicio.js
+++ Fundamentos/bloco10/exercicios/dia-3-jest-simulando-comportamentos/exercicio.js
@@ -1,4 +1,8 @@
 const randomNumber = () => {
   return Math.round(Math.random() * 100);
 };
-module.exports = { randomNumber };
+const func1 = (string) => string.toUpperCase();
+const func2 = (string) => string[0];
+const func3 = (string1, string2) => string1 + string2;
+
+module.exports = { randomNumber, func1, func2, func3 };
--- Fundamentos/bloco10/exercicios/dia-3-jest-simulando-comportamentos/exercicio.test.js
+++ Fundamentos/bloco10/exercicios/dia-3-jest-simulando-comportamentos/exercicio.test.js
@@ -20,4 +20,21 @@ describe('exercises', () => {
     exercicios.randomNumber.mockReset();
     expect(exercicios.randomNumber()).not.toBe(101);
   });
+  describe('exercicio4', () => {
+    it('func1', () => {
+      expect(exercicios.func1('olaa')).toBe('OLAA');
+      exercicios.func1 = jest.fn().mockImplementationOnce((string) => string.toLowerCase());
+      expect(exercicios.func1('OLAA')).toBe('olaa');
+    });
+    it('func2', () => {
+      expect(exercicios.func2('string')).toBe('s');
+      exercicios.func2 = jest.fn().mockImplementation((string) => string[string.length - 1]);
+      expect(exercicios.func2('string')).toBe('g');
+    });
+    it('func3', () => {
+      expect(exercicios.func3('hello', ' world')).toBe('hello world');
+      exercicios.func3 = jest.fn().mockImplementation((a, b, c) => a + b + c);
+      expect(exercicios.func3('azy', 'bzy', 'czy')).toBe('azybzyczy')
+    });
+  });
 });
