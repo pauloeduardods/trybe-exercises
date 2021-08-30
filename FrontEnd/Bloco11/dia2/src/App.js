43c3466bbedd97dfc3f260c9c0cd2d12ebd27ca0
Paulo Eduardo de Sordi Gomes <pauloeduardodesordigomes@gmail.com>
2021-08-30 17:06:19 -0300

    Add width props 2

--- FrontEnd/Bloco11/dia2/src/App.js
+++ FrontEnd/Bloco11/dia2/src/App.js
@@ -3,7 +3,7 @@ import Image from "./Image";
 export default function App() {
   return (
     <div>
-      <Image source="https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jpg" alt="Cute cat staring" />
+      <Image source="https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jpg" alt="Cute cat staring" width="230px" />
     </div>
   )
 }
