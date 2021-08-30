a450f4d950099639ba3d820c64ba52099c7992d4
Paulo Eduardo de Sordi Gomes <pauloeduardodesordigomes@gmail.com>
2021-08-30 18:46:57 -0300

    Add components exercise

---
+++ FrontEnd/Bloco11/dia2-components/src/App.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import Order from './Order';
+
+class App extends React.Component {
+  render() {
+    const headphone = {
+      id: 102,
+      user: "cena@gmail.com",
+      product: "Razer Headphone",
+      price: {
+        value: 99.99,
+        currency: "dollars"
+      }
+    };
+
+    const energyDrink = {
+      id: 77,
+      user: "cena@gmail.com",
+      product: "Monster 500mL",
+      price: {
+        value: 9.99,
+        currency: "dollars"
+      }
+    };
+
+    return (
+      <div className="App">
+        <h1> Orders recently created </h1>
+        {/* <Order user={headphone.user} product={headphone.product} price={headphone.price} /> */}
+        <Order order={({ user: energyDrink.user, product: energyDrink.product, price: energyDrink.price})}/>
+      </div>
+    );
+  }
+}
+
+export default App;
