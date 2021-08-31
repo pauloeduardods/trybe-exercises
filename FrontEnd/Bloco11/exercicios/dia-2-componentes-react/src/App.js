a0e6b9001c5f831f354d80fc153a68ecc50ca0c2
Paulo Eduardo de Sordi Gomes <pauloeduardodesordigomes@gmail.com>
2021-08-31 10:45:05 -0300

    Import pokemon function in App.js

--- FrontEnd/Bloco11/exercicios/dia-2-componentes-react/src/App.js
+++ FrontEnd/Bloco11/exercicios/dia-2-componentes-react/src/App.js
@@ -1,10 +1,11 @@
 import pokemons from "./data";
+import DisplayPokemon from "./displayPokemon";
 
 export default function App() {
   return (
     <>
       <section>
-        
+
       </section>
     </>
   )
