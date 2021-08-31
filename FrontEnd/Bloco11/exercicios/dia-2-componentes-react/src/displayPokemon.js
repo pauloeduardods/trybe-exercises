7084c7f67b454252d41aed27442e8d24fc5966c4
Paulo Eduardo de Sordi Gomes <pauloeduardodesordigomes@gmail.com>
2021-08-31 11:53:03 -0300

    Add borders to pokemons

--- FrontEnd/Bloco11/exercicios/dia-2-componentes-react/src/displayPokemon.js
+++ FrontEnd/Bloco11/exercicios/dia-2-componentes-react/src/displayPokemon.js
@@ -3,23 +3,25 @@ import PropTypes from 'prop-types';
 
 function processObject({ name, type, averageWeight, image, moreInfo }) {
   return (
-  <div className="col-sm-6 col-lg-4">
-    <a href={moreInfo} className="row">
-      <div className="col-7">
-        <p className="fs-4">{name}</p>
-        <p className="fs-6">{type}</p>
-        <p className="fs-6">Average Weight: {`${averageWeight.value} ${averageWeight.measurementUnit}`}</p>
+    <div className="col-sm-6 col-lg-4">
+      <div className="mx-1 my-2 rounded-3 border border-2 border-black" >
+        <a href={moreInfo} className="container row">
+          <div className="col-7">
+            <p className="fs-4">{name}</p>
+            <p className="fs-6">{type}</p>
+            <p className="fs-6">Average Weight: {`${averageWeight.value} ${averageWeight.measurementUnit}`}</p>
+          </div>
+          <img className="col-5" src={image} alt={`${name} Image`}></img>
+        </a>
       </div>
-      <img className="col-5" src={image} alt={`${name} Image`}></img>
-    </a>
-  </div>
+    </div>
   );
 }
 
 export default class DisplayPokemon extends Component {
   render() {
     return (
-    <section className="container row">
+    <section className="row">
         {this.props.pokemons.map((pokemon) => processObject(pokemon))}
     </section>
     );
