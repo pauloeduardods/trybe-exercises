ae56ac75810143e4924794d6a98f5e3a025a906e
Paulo Eduardo de Sordi Gomes <pauloeduardodesordigomes@gmail.com>
2021-09-02 16:02:18 -0300

    Add exercise 8- 9

--- FrontEnd/Bloco12/dia1/src/components/buttons.jsx
+++ FrontEnd/Bloco12/dia1/src/components/buttons.jsx
@@ -5,25 +5,39 @@ class Button extends React.Component {
     super();
     this.state = {
       clickNum: 0,
+      color: { backgroundColor: 'white' },
     }
     this.sum = this.sum.bind(this);
     this.subtract = this.subtract.bind(this);
+    this.verifier = this.verifier.bind(this);
   }
-  sum() {
-    this.setState((lastState) => ({
+  verifier() {
+    let color = { backgroundColor: 'Red' };
+    if (this.state.clickNum % 2 === 0) {
+      color.backgroundColor = 'green';
+    } else if (this.state.clickNum <= 0) {
+      color.backgroundColor = 'blue'
+    }
+    return color;
+  }
+   sum() {
+    this.setState((lastState) =>  ({
       clickNum: lastState.clickNum + 1,
-    }));
+      color: this.verifier(),
+      })
+    );
   }
   subtract() {
     this.setState((lastState) => ({
       clickNum: lastState.clickNum - 1,
+      color: this.verifier(),
     }));
   }
   render() {
     return (
       <div>
-        <input type="button" onClick={this.sum} value={this.state.clickNum} />
-        <input type="button" onClick={this.subtract} value={this.state.clickNum} />
+        <input type="button" style={this.state.color} onClick={this.sum} value={this.state.clickNum} />
+        <input type="button" style={this.state.color} onClick={this.subtract} value={this.state.clickNum} />
       </div>
     );
   }
