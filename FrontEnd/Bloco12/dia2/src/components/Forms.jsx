5e150ea20c9cd609d59ff0aab2d92a4c8d7cc17f
Paulo Eduardo de Sordi Gomes <pauloeduardodesordigomes@gmail.com>
2021-09-03 15:18:56 -0300

    Exercise 6

--- FrontEnd/Bloco12/dia2/src/components/Forms.jsx
+++ FrontEnd/Bloco12/dia2/src/components/Forms.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
 
 class Forms extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.saveChange = this.saveChange.bind(this);
+    this.fileInput = React.createRef();
     this.state = {
       name: '',
       age: 0,
       email: '',
       password: '',
       remember: false,
+      file: '',
     }
   }
   saveChange({target}) {
@@ -18,18 +20,22 @@ class Forms extends React.Component {
     this.setState({
       [name]: value
     });
+    console.log(this.fileInput.current.files[0])
   }
   render() {
     return (
       <section onChange={this.saveChange}>
-        <input type="text" name="name" placeholder="Name" />
-        <input type="email"  name="email" placeholder="Email" />
-        <input type="number" name="age" placeholder="Age" />
-        <input type="password"  name="password" placeholder="Password" />
+        <fieldset>
+          <input type="text" name="name" placeholder="Name" />
+          <input type="email" name="email" placeholder="Email" />
+          <input type="number" name="age" placeholder="Age" />
+          <input type="password" name="password" placeholder="Password" />
+        </fieldset>
         <label>
           <input type="checkbox" name="remember" />
           Lembrar
         </label>
+        <input type="file" name="file" ref={this.fileInput} />
       </section>
     )
   }
