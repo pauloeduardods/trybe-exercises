4792c6ecbe0751cb6ad3a40f04ae18df3c054175
Paulo Eduardo de Sordi Gomes <pauloeduardodesordigomes@gmail.com>
2021-09-04 17:33:52 -0300

    Displat states and city

--- FrontEnd/Bloco12/exercicios/dia2/src/components/Forms.jsx
+++ FrontEnd/Bloco12/exercicios/dia2/src/components/Forms.jsx
@@ -1,11 +1,33 @@
 import React from 'react';
 
-class Selector extends React.Component {
+class StateSelector extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+     this.state = {
+      state: props.states.states[0].uf,
+      city: ''
+    };
+  }
+  handleChange({ target }) {
+    this.setState({ [target.name]: target.value })
+  }
   render() { 
+    const { states } = this.props;
+    //console.log(states.states.filter((curState) => state ? curState.state === state : true));
     return (
-      <select name={this.props.name}>
-        {this.props.states.states.map((state) => <option value={state} key={state}>{Object.values(state)[0]}</option>)}
-      </select>
+      <div>
+        <label htmlFor="state">Estado: </label>
+        <select id="state" name="state" onChange={this.handleChange} >
+          {states.states.map((state) => <option name="state" value={state.uf} key={`${state.name}-${state.uf}`}>{`${state.name}: ${state.uf}`}</option>)}
+        </select>
+        <label htmlFor="city">Cidade: </label>
+        <select name="city" id="city">
+          { states.states
+            .find((state) => state.uf === this.state.state).cities
+            .map((cur, index) => <option value={cur} key={`${cur}-${index}`}>{cur}</option>) }
+        </select>
+      </div>
     );
   }
 }
@@ -26,7 +48,7 @@ class Forms extends React.Component {
           <input name="address" id="address" placeholder="Endereco" maxLength="200" required />
           <label htmlFor="city">Cidade: </label>
           <input name="city" id="city" placeholder="Cidade" maxLength="28" required/>
-          <Selector states={this.props.statesObj} name="state" />
+          <StateSelector states={this.props.statesObj} />
         </fieldset>
       </form>
     );
